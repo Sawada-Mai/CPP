b0c8bae2cd4334746574317e9f2c01e843560d24
msawada <sawadamai@sawadamainoMac-mini.local>
2025-09-07 11:09:19 +0900

    ADD: CPP00/ex00

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+static void print_upper(char *str)
+{
+	while (*str)
+	{
+		std::cout << static_cast<unsigned char>(std::toupper(*str));
+		str++;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	i = 1;
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		return (0);
+	}
+	while (i < argc)
+	{
+		print_upper(argv[i]);
+		i++;
+	}
+	std::cout << '\n';
+	return (0);
+}
