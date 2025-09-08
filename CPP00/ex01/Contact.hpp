74888a7d5c4bdff46a6bf719d2ed34d3bf55ef16
msawada <sawadamai@sawadamainoMac-mini.local>
2025-09-09 00:16:31 +0900

    STYLE: space to tab

--- CPP00/ex01/Contact.hpp
+++ CPP00/ex01/Contact.hpp
@@ -12,23 +12,22 @@ class Contact
 		std::string	last_name_;
 		std::string	nickname_;
 		std::string	phone_number_;
-		std::string darkest_secret_;
+		std::string	darkest_secret_;
 
 	public:
 		Contact();
 		~Contact();
-		void set_first_name(const std::string& first_name);
-		void set_last_name(const std::string& last_name);
-		void set_nickname(const std::string& nickname);
-		void set_phone_number(const std::string& phone_number);
-		void set_darkest_secret(const std::string& darkest_secret);
-		const std::string& get_first_name() const;
-		const std::string& get_last_name() const;
-		const std::string& get_nickname() const;
-		const std::string& get_phone_number() const;
-		const std::string& get_darkest_secret() const;
+		void	set_first_name(const std::string& first_name);
+		void	set_last_name(const std::string& last_name);
+		void	set_nickname(const std::string& nickname);
+		void	set_phone_number(const std::string& phone_number);
+		void	set_darkest_secret(const std::string& darkest_secret);
+		const std::string&	get_first_name() const;
+		const std::string&	get_last_name() const;
+		const std::string&	get_nickname() const;
+		const std::string&	get_phone_number() const;
+		const std::string&	get_darkest_secret() const;
 };
-
 
 
 #endif
