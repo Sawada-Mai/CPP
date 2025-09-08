8be096478134286dd3edce81567eb56d823c666e
msawada <sawadamai@sawadamainoMac-mini.local>
2025-09-08 22:23:08 +0900

    ADD: Contact class and function

---
+++ CPP00/ex01/Contact.hpp
@@ -0,0 +1,34 @@
+#ifndef CONTACT_H
+#define CONTACT_H
+
+#include <iostream>
+#include <string>
+
+
+class Contact
+{
+	private:
+		std::string	first_name_;
+		std::string	last_name_;
+		std::string	nickname_;
+		std::string	phone_number_;
+		std::string darkest_secret_;
+
+	public:
+		Contact();
+		~Contact();
+		void set_first_name(const std::string& first_name);
+		void set_last_name(const std::string& last_name);
+		void set_nickname(const std::string& nickname);
+		void set_phone_number(const std::string& phone_number);
+		void set_darkest_secret(const std::string& darkest_secret);
+		const std::string& get_first_name() const;
+		const std::string& get_last_name() const;
+		const std::string& get_nickname() const;
+		const std::string& get_phone_number() const;
+		const std::string& get_darkest_secret() const;
+};
+
+
+
+#endif
