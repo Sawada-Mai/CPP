178d0bc952a3e9937bf4cd206db941b1fd03bf71
msawada <sawadamai@sawadamainoMac-mini.local>
2025-09-14 13:39:29 +0900

    STYLE: adjust function brace placement

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -1,29 +1,25 @@
 #include <iostream>
 
-static void print_upper(char *str)
-{
-	while (*str)
-	{
+static void	print_upper(char *str) {
+	while (*str) {
 		std::cout << static_cast<char>(std::toupper(*str));
 		str++;
 	}
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
 	int i;
 
 	i = 1;
-	if (argc == 1)
-	{
+	if (argc == 1) {
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-		return (0);
+		return 0;
 	}
-	while (i < argc)
-	{
+	while (i < argc) {
 		print_upper(argv[i]);
 		i++;
 	}
 	std::cout << '\n';
-	return (0);
+	return 0;
 }
