1d6c7f6ffced7edd7edfd4c750ee700992b60834
msawada <sawadamai@sawadamainoMac-mini.local>
2025-10-07 16:57:32 +0900

    EDIT: CPP01 ex04

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -5,23 +5,36 @@
 #include <iostream>
 
 
-int main(void) {
+std::string ReplaceLine(const std::string& line,
+    const std::string& s1, const std::string& s2) {
+  if (s1.empty() || s2.empty() || s1 == s2)
+    return NULL;
+  size_t i = 0;
+  size_t hit;
+  std::string result;
+  while ((hit = line.find(s1, i)) != std::string::npos) {
+    result += line.substr(i, hit - i);
+    result += s2;
+    i = hit + s1.size();
+  }
+  result += line.substr(i);
+  return result;
+}
+
+int main(int argc, char** argv) {
   std::ifstream ifs("test.txt");
   std::ofstream file("test.replace");
-
-  if (!ifs) {
-    std::cerr << "Error: file can not open.";
+  if (argc != 3) {
     return 1;
   }
-  if (!file) {
+  if (!ifs || !file) {
     std::cerr << "Error: file can not open.";
     return 1;
   }
   std::string line;
   std::string str;
   while (std::getline(ifs, line)) {
-    std::cout << line << std::endl;
-    str = MakeFileLine(line);
-    file << str << std::endl;
+    str = ReplaceLine(line, argv[1], argv[2]);
   }
+  return 0;
 }
